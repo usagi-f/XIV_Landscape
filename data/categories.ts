564759ede15d7ccca50eef57fd813bf248b5e669
usagi.f <usagi.f@gmail.com>
2020-08-11 02:09:11 +0900

    add: categories

--- data/categories.ts
+++ data/categories.ts
@@ -5,5 +5,76 @@ export const categories = [
   'The_Black_Shroud',
   'Coerthas',
   'Mor_Dhona',
-  'Abalathia'
-] as const // and more.
+  'Dravania',
+  'Abalathia',
+  'Gyr_Abania',
+  'Othard',
+  'Norvrandt'
+] as const
+
+/*
+# La_Noscea
+- Limsa Lominsa
+- Lower La Noscea
+- Middle La Noscea
+- Eastern La Noscea
+- Western La Noscea
+- Upper La Noscea
+- Outer La Noscea
+
+# Thanalan
+- Ul'dah
+- Central Thanalan
+- Western Thanalan
+- Eastern Thanalan
+- Southern Thanalan
+- Northern Thanalan
+
+# The_Black_Shroud
+- Gridania
+- Central Shroud
+- North Shroud
+- East Shroud
+- South Shroud
+
+# Coerthas
+- Ishgard
+- Coerthas Central Highlands
+- Coerthas Western Highlands
+
+# Mor_Dhona
+- Mor Dhona
+
+# Dravania
+- Idyllshire
+- The Dravanian Forelands
+- The Dravanian Hinterlands
+- The Churning Mists
+
+# Abalathia
+- The Sea of Clouds
+- Azys Lla
+
+# Gyr_Abania
+- Rhalgr's Reach
+- The Fringes
+- The Lochs
+- The Peaks
+
+# Othard
+- Kugane
+- The Azim Steppe
+- The Ruby Sea
+- Yanxia
+
+# Norvrand
+- Crystarium
+- Lakeland
+- Eulmore
+- Kholusia
+- Amh Araeng
+- Il Mheg
+- The Rak'tika Greatwood
+- The Tempest
+
+*/
