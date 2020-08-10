503ddb9d4b3ba4a2375dd2d127fb0d965e582148
usagi.f <usagi.f@gmail.com>
2020-08-11 02:58:44 +0900

    add: images

--- data/images-data.ts
+++ data/images-data.ts
@@ -3,110 +3,26 @@ import { ScreenShotType } from '../interfaces'
 export const imagesData: ScreenShotType[] = [
   {
     file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
+    group: 'Dravania',
   },
   {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
+    file: 'ffxiv_20191222_130718_299.jpg',
+    group: 'Othard',
   },
   {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
+    file: 'ffxiv_20190715_021115_087.jpg',
+    group: 'Abalathia',
   },
   {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
+    file: 'ffxiv_20190715_004123_123.jpg',
+    group: 'Coerthas',
   },
   {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
+    file: 'ffxiv_20200809_210459_682.jpg',
+    group: 'Coerthas',
   },
   {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'La_Noscea',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'Thanalan',
-  },
-  {
-    file: 'ffxiv_20191103_204202_511.jpg',
-    group: 'The_Black_Shroud',
+    file: 'ffxiv_20190707_225257_855.jpg',
+    group: 'Norvrandt',
   },
 ];
