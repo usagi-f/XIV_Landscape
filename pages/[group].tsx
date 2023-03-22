d325b27c7ea6fabb320ed57923bef0f723cc101e
Keita Ishibashi <usagi.f@gmail.com>
2023-03-22 13:17:05 +0000

    feat: change to ssr

--- pages/[group].tsx
+++ pages/[group].tsx
@@ -1,58 +1,38 @@
-import { GetStaticProps, GetStaticPaths } from 'next';
+import { GetServerSideProps } from 'next';
 import { ScreenShotType, Group } from '../interfaces';
 import Layout from '../components/Layout';
 import List from '../components/List';
-import { categories } from '../data/categories';
 import { endpoint } from './api/const';
 import { shuffle } from '../utils/shuffle';
 
 type Props = {
-  group?: Group;
-  images?: ScreenShotType[];
-  errors?: string;
+  group: Group;
+  images: ScreenShotType[];
 };
 
-const StaticPropsDetail = ({ group, images, errors }: Props) => {
-  if (errors || typeof images === 'undefined') {
-    return (
-      <Layout title={`${group || 'Error'} > XIV_Landscape`}>
-        <p style={{ width: '100%', textAlign: 'center' }}>{errors}</p>
-      </Layout>
-    );
-  }
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const group = params?.group;
+  const json = await fetch(
+    `${endpoint}/images?group=${group}}`
+  )
+    .then((res) => res.json())
+    .catch(() => []);
+  return {
+    props: {
+      group,
+      images: shuffle(json),
+    },
+  };
+};
 
-  return (
-    <Layout title={`${group || 'None Group'} > XIV_Landscape`}>
+const Group = ({ group, images }: Props) => (
+  <Layout title={`${group} > XIV_Landscape`}>
+    {images.length ? (
       <List images={images} />
-    </Layout>
-  );
-};
+    ) : (
+      <p style={{ width: '100%', textAlign: 'center' }}>There is no image</p>
+    )}
+  </Layout>
+);
 
-export default StaticPropsDetail;
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = categories.map((category) => `/${category}`);
-  return { paths, fallback: true };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  try {
-    const group = params?.group;
-    console.log(params);
-    const res = await fetch(
-      `${endpoint}/images${group ? `?group=${group}` : ''}`
-    );
-    const json = await res.json();
-    const images: ScreenShotType[] = shuffle(json);
-    if (images.length === 0)
-      return { props: { group, errors: 'There is no image' } };
-    return {
-      props: {
-        group,
-        images,
-      },
-    };
-  } catch (err) {
-    return { props: { errors: (err as any).message } };
-  }
-};
+export default Group;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { ScreenShotType } from '../interfaces';
 import Layout from '../components/Layout';
 import List from '../components/List';
@@ -9,18 +9,17 @@ type Props = {
   images: ScreenShotType[];
 };
 
-const WithStaticProps = ({ images }: Props) => (
+export const getServerSideProps: GetServerSideProps = async () => {
+  const json = await fetch(`${endpoint}/images`)
+    .then((res) => res.json())
+    .catch(() => []);
+  return { props: { images: shuffle(json) } };
+};
+
+const Index = ({ images }: Props) => (
   <Layout title="> XIV_Landscape">
     <List images={images} />
   </Layout>
 );
 
-export const getStaticProps: GetStaticProps = async () => {
-  const json = await fetch(`${endpoint}/images`)
-    .then((res) => res.json())
-    .catch(() => []);
-  const images = shuffle(json);
-  return { props: { images } };
-};
-
-export default WithStaticProps;
+export default Index;
